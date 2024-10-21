52a6be29050d7ffe31d35ee6383ae9c37a60b801
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-10-21 14:21:42 -0400

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "Programming is like building
+ * a multilingual puzzle" followed by a new line using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 
