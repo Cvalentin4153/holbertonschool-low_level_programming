fa2247459d93478aef3ff913e539b6adb032be89
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-10-21 14:28:34 -0400

    Task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "with proper grammar, but the outcome
+ * is a piece of art," followed by a new line using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void){
 
 printf("with proper grammar, but the outcome is a piece of art,\n");
