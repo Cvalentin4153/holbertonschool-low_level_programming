605e0eb1c96dc7490e1dfcb88e172ef2cf3ab0b4
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-10-22 11:17:13 -0400

    Task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -22,19 +22,19 @@ int main(void)
 if (n > 0)
 {
 
-	printf("is positive\n");
+	printf("%d is positive\n", n);
 
 }
 else if (n == 0) 
 {
 
-	printf("is zero\n");
+	printf("%d is zero\n", n);
 
 }
 else
 {
 
-	printf("is positive\n");
+	printf("%d is positive\n", n);
 
 }
 
