1ac75fcedbf44a249b84882e19362d6ccbb21bb7
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-10-25 14:11:49 -0400

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,53 +7,32 @@
 */
 
 void times_table(void)
-
 {
-
 int n;
 int l;
 int mul;
-
 for (n = 0; n <= 9; n++)
-
 {
-
-
 for (l = 0; l <= 9; l++)
-
 {
-
 mul = n * l;
-
 if (mul < 10)
 {
-
 if (l != 0)
-
 _putchar(' ');
-
 _putchar(mul + '0');
-
 }
-
 else
 {
-
 _putchar((mul / 10) + '0');
 _putchar((mul % 10) + '0');
-
 }
-
 if (l != 9)
 {
-
 _putchar(',');
 _putchar(' ');
-
 }
 }
-
 _putchar('\n');
-
 }
 }
