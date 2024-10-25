b02662fc551d34e423e215aed94778e9e185a864
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-10-24 23:16:20 -0400

    Task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - Checks if a character is lowercase
+ * @c: The character to check (represented as an integer)
+ *
+ * Description: This function checks if the provided character `c`
+ *              is a lowercase letter ('a' through 'z').
+ *
+ * Return: 1 if `c` is lowercase, 0 otherwise
+ */
+int _islower(int c)
+
+{
+if (c >= 97 && c <= 122)
+return (1);
+
+else 
+
+return (0);
+
+}
