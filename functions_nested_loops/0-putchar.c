46a3d01cf8f6be56e883fc74c24a667b7a9995ca
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-10-24 20:51:29 -0400

    Task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _putchar - Custom function to output a character
+ * @c: The character to output
+ *
+ * Return: 0 on success, or an error code
+ */
+int main(void)
+
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H 
+#define MAIN_H 
+
+/**
+ * _putchar - Custom function to output a character
+ * @c: The character to be printed
+ *
+ * Return: 0 on success, or an error code on failure
+ */
+
+int _putchar(char c);
+
+#endif /*MAIN_H*/
