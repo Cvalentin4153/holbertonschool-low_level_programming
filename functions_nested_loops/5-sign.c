f17bd6bda1021b2469ee7fe51948f05b1e838d59
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-10-24 23:44:37 -0400

    Task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,10 +3,10 @@
  * print_sign - Prints the sign of a number
  * @n: The number to check
  *
- * Description: This function checks if a number is positive, zero, or negative.
- *              It prints '+' if the number is positive, '0' if the number is zero,
- *              and '-' if the number is negative. It also returns 1, 0, or -1
- *              accordingly.
+ * Description:This function checks if a number is positive, zero, or negative.
+ * It prints '+' if the number is positive, '0' if the number is zero,
+ * and '-' if the number is negative. It also returns 1, 0, or -1
+ * accordingly.
  *
  * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative
  */
