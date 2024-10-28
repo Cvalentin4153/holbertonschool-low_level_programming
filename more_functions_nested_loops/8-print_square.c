75bf224f9c7276ab2f83aa89ce2afb455a07b6c6
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-10-28 14:42:49 -0400

    Task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * print_square - Prints a square of size `size` using the character '#'
+ * @size: The dimension of the square (both width and height)
+ *
+ * Return: void
+ */
 void print_square(int size)
 {
 int s;
@@ -17,7 +22,6 @@ int a;
 			{
 				_putchar('#');
 			}
-			_putchar('#');
 			_putchar('\n');
 		}
 	}
