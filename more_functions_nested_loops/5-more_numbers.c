0431107b2b91053d1421f178bff6aa1ee2fd6406
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-10-28 12:21:02 -0400

    Task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,10 +6,15 @@ void more_numbers(void)
 	int i;
 
 	for (i = 0; i <= 10; i++)
-			for (num = 0; num <= 14; num++)
+	{
+		for (num = 0; num <= 14; num++)
+			{
+				if (num > 9)
 				{
-				_putchar(num + '0');
-				_putchar('\n');
+					_putchar((num / 10) + '0');
+					_putchar((num % 10) + '0');
 				}
+			}
+	}
 	_putchar('\n');
 }
