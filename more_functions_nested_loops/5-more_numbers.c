dd3a75778e1e7a74c5cbd455b7776c338fc797d7
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-10-28 12:59:24 -0400

    Task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,24 @@
 #include "main.h"
-
+/**
+ * more_numbers - Prints numbers from 0 to 14, ten times
+ *
+ * Return: void
+ */
 void more_numbers(void)
 {
 	int num;
 	int i;
 
-	for (i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
+{
+	for (num = 0; num <= 14; num++)
 	{
-		for (num = 0; num <= 14; num++)
-			{
-				if (num > 9)
-					{
-						_putchar((num / 10) + '0');
-					}
-				_putchar((num % 10) + '0');
-			}
-		_putchar('\n');
+		if (num > 9)
+		{
+			_putchar((num / 10) + '0');
+		}
+	_putchar((num % 10) + '0');
 	}
+_putchar('\n');
+}
 }
