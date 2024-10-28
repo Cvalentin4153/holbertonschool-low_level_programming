2f0c9b142c162cc9e51d5fb4bf30d3bcbc8ef555
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-10-28 11:56:55 -0400

    Task 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,8 @@
 #include "main.h"
- /**
- *
+/**
  * print_most_numbers - Prints numbers from 0 to 9, except 2 and 4.
  *
- * Return - void.
- *
+ * Return: void
  */
 void print_most_numbers(void)
 
