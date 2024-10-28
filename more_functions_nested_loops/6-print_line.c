3ff6935f1d07053582baff0e74afcf9be6ea1e24
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-10-28 13:55:28 -0400

    Task 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,22 @@
 #include "main.h"
-
+/**
+ * print_line - Draws a straight line in the terminal
+ * @n: The number of times the character _ should be printed
+ *
+ * Return: void
+ */
 void print_line(int n)
 {
-char l='_';
-
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-		while (n > 0)
+	else
+	{
+		for (int l = 0; l < n; l++)
 		{
-			_putchar(l);
+			_putchar('_');
 		}
 	_putchar('\n');
+	}
 }
