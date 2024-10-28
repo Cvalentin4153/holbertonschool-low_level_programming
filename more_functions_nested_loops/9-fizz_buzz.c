086e26f0e9c715624a947043d2ef8bdf15290d40
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-10-28 15:10:10 -0400

    Task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
-#include "main.h"
-#include <stdlib.h>
-
+/**
+ * main - Prints the numbers from 1 to 100 with FizzBuzz rules:
+ *        Prints "Fizz" for multiples of 3, "Buzz" for multiples of 5,
+ *        and "FizzBuzz" for multiples of both 3 and 5.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
 
 		for (i = 1; i <= 100; i++)
 		{
-			if (i % 3 == 0)
+			if (i % 15 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (i % 3 == 0)
 			{
 				printf("Fizz ");
 			}
 			else if (i % 5 == 0)
 			{
 				printf("Buzz ");
-			}
-			else if (i % 15 == 0)
-			{
-				printf("FizzBuzz ");
 			}
 			else
 				printf("%d ", i);
