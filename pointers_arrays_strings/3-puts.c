42fc6082bca517dd547c50d8affb915d3d579fad
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-10-30 16:03:16 -0400

    Task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: pointer to the string to print
+ *
+ * Description: This function takes a pointer to a character array (string)
+ * and prints each character one by one until it reaches the null terminator.
+ * After printing the string, it outputs a newline character.
+ */
 void _puts(char *str)
 {
 	while (*str != '\0')
