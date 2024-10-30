f75c800c6316211617328e5ad00bec36e6c4619a
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-10-30 14:31:58 -0400

    Task 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,21 @@
 #include "main.h"
-
+/**
+ * _strlen - Returns the length of a string
+ * @s: pointer to the string (character array)
+ *
+ * Description: This function takes a pointer to a character array (string)
+ * and returns the number of characters in the string, excluding the null
+ * terminator.
+ *
+ * Return: length of the string as an integer
+ */
 int _strlen(char *s)
 {
-return (*s);
+int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
 }
