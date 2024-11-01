361c4e2b36ec2fe1aa04c1080a86fac1af7adab6
Carlos Valentin <carlosvalentin@Carloss-MBP.lan>
2024-11-01 15:16:17 -0400

    Task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+int length = 0;
+int start = 0;
+int end;
+char temp;
+
+
+	while (s[length] != '\0')
+	{
+	length++;
+	}
+		end = length -  1;
+			while (start < end)
+			{
+				temp = s[start];
+				s[start] = s[end];
+				s[end] = temp;
+					start++;
+					end--;
+			}
+}
