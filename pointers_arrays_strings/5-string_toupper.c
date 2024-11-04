a6705bebaca83bb3054bc89d773e8448033b69de
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-11-03 21:23:35 -0400

    Task 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -12,15 +12,14 @@
  */
 char *string_toupper(char *str)
 {
-int i = 0;
+int i;
 
-	while (i != '\0')
+	for (i = 0; i != '\0'; i++)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
 			str[i] = str[i] - 32;
 		}
-		i++;
 	}
 	return (str);
 }
