48e811021f3420dd30921dabf175f8ba8c8b9095
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-11-03 21:51:59 -0400

    Task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: pointer to the string to be modified
+ *
+ * Description: This function capitalizes each word in the input string.
+ * Words are separated by spaces, tabulation, new lines, or punctuation marks.
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+int i = 0;
+char separators[] = "\t\n,;.!?\"(){}";
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = 'A' + (str[i] - 'a');
+	}
+		while (str[i] != '\0')
+		{
+		int j;
+			for (j = 0; separators[j] != '\0'; j++)
+			{
+				if (str[i] == separators[j] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+				{
+					str[i + 1] = 'A' + (str[i + 1] - 'a');
+					break;
+				}
+			}
+			i++;
+		}
+		return (str);
+}
