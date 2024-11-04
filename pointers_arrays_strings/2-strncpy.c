4695f9e28d36ea94355ea1fb7f47bd2271f25d56
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-11-03 20:19:16 -0400

    Task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,16 @@
 #include "main.h"
-
+/**
+ * _strncpy - Copies a string, up to n characters
+ * @dest: pointer to the destination array where the content is to be copied
+ * @src: pointer to the source string to be copied
+ * @n: maximum number of characters to copy from src
+ *
+ * Description: This function copies at most n characters from src to dest.
+ * If src has fewer than n characters, the rest of dest will be padded with
+ * null bytes. If src has n or more characters, dest will not be null-terminated.
+ *
+ * Return: pointer to the destination string dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
