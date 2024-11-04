1f2994dd3581716b3534f051da670fb0b5c13fe3
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-11-03 21:13:48 -0400

    Task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase
+ * @str: pointer to the string to be modified
+ *
+ * Description: This function takes a string and converts all lowercase
+ * letters to uppercase. It iterates through each character, and if the
+ * character is a lowercase letter, it converts it by subtracting 32 from
+ * its ASCII value.
+ *
+ * Return: pointer to the modified string
+ */
+char *string_toupper(char *str)
+{
+int i;
+
+for (i = 0; i != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
