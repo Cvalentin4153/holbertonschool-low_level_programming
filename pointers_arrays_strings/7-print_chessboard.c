9260055b435d4543d1ed76f5a5879f4e5dd63fda
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-11-07 18:16:23 -0400

    Task 6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0; row <= 8; row++)
+	{
+		col = 0;
+		for (col = 0; col <= 8; col++)
+		{
+		_putchar(a[row][col]);
+		}
+		_putchar('\n');
+	}
+}
