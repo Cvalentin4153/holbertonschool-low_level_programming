8226db2aa800f24c2ab1cac6fe7eb772ea8673d0
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-11-08 16:04:58 -0400

    Task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,11 +4,23 @@
  * @n: The number to find the square root of
  *
  * Description: Uses recursion to determine the natural square root
- * of a given number. If the number does not have a natural square
- * root, returns -1.
+ * of a given number by calling a helper function. If the number does
+ * not have a natural square root, returns -1.
  *
  * Return: The natural square root of n, or -1 if n does not have
  * a natural square root
+ */
+/**
+ * helper - Recursively checks for the natural square root of n
+ * @n: The number to find the square root of
+ * @i: The current number being tested as the square root
+ *
+ * Description: Checks if the square of the current number i equals n,
+ * indicating it’s the square root. If the square of i exceeds n,
+ * returns -1. Otherwise, increments i and continues the check.
+ *
+ * Return: The natural square root of n, or -1 if no natural
+ * square root exists
  */
 int helper(int n, int i)
 {
@@ -22,7 +34,7 @@ int helper(int n, int i)
 	}
 	else
 	{
-		return helper(n, i + 1);
+		return (helper(n, i + 1));
 	}
 }
 int _sqrt_recursion(int n)
@@ -33,6 +45,6 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
-		return helper(n, 1);
+		return (helper(n, 1));
 	}
 }
