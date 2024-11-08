4b47daf512ae337b689fa4bda12f5398e39b4217
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-11-08 16:14:10 -0400

    Task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -13,18 +13,18 @@
  */
 int _helper(int n, int i)
 {
-    if (i * i == n)
-    {
-        return (i);
-    }
-    else if (i * i > n)
-    {
-        return (-1);
-    }
-    else
-    {
-        return (_helper(n, i + 1));
-    }
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_helper(n, i + 1));
+	}
 }
 /**
  * _sqrt_recursion - Returns the natural square root of a number
@@ -39,12 +39,12 @@ int _helper(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
-    else
-    {
-        return (_helper(n, 1));
-    }
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_helper(n, 1));
+	}
 }
