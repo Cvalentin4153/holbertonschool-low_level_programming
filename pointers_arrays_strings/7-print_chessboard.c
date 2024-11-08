0ce3bdb907bba6f49c5af5ece22debcf3464523f
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-11-08 09:48:50 -0400

    Task 6

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * print_chessboard - Prints an 8x8 chessboard
+ * @a: 2D array representing the chessboard
+ *
+ * Description: This function prints an 8x8 chessboard represented by
+ * a 2D array of characters. Each element in the array represents a square
+ * on the board. After each row, a newline is printed to format the board.
+ */
 void print_chessboard(char (*a)[8])
 {
 	int row;
