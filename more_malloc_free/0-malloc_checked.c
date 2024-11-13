e1ffcd8b81502ca0fedaba91d0386507673ac98a
Carlos Valentin <carlosvalentin@Carloss-MacBook-Pro.local>
2024-11-13 06:40:44 -0400

    Task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the number of bytes to allocate
+ *
+ * Return: pointer to the allocated memory
+ *         If malloc fails, the function terminates with a status value of 98.
+ */
 void *malloc_checked(unsigned int b)
 {
 void *ptr;
