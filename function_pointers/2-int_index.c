d57f6a5cb0e8e537fd6f598d561283d1fcbecede
Carlos Valentin <carlosvalentin@Carloss-MBP.localdomain>
2024-11-19 11:32:53 -0400

    Task 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+	}
+		if (i != 0)
+			{
+				return (i);
+			}
+			else
+			{
+				return (-1);
+			}
+}
