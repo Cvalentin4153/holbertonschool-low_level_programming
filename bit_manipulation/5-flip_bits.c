dd76fcc88d5914d42d1ba1c360cf23508c50bc36
cvalentin4153 <cvalentin4153@outlook.com>
2024-12-08 16:50:41 -0400

    Task 5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - Calculates the number of bits needed to flip to convert n to m.
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits to flip.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m;
+	unsigned int count = 0;
+
+	while (xor)
+	{
+		count += xor & 1;
+		xor >>= 1;
+	}
+	return (count);
+}
